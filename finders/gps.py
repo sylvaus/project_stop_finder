6dbafc20b24b16abc1886cbe8a72fd9e8ba04b35
Thanyte <50961577+Thanyte@users.noreply.github.com>
2019-11-15 09:03:43 -0500

    Adding gps_distance
    Calcul from https://geodesie.ign.fr/contenu/fichiers/Distance_longitude_latitude.pdf

--- finders/gps.py
+++ finders/gps.py
@@ -1,8 +1,20 @@
 from dataclasses import dataclass
-
+from math import sin, cos, acos, pi, radians
 
 @dataclass
 class GPSPosition:
     latitude: float
     longitude: float
 
+def gps_distance(Start : GPSPosition,End : GPSPosition):
+    # Rayon de la terre en mètres (sphère IAG-GRS80)
+    RT = 6378137
+
+    distance = acos(
+        sin(radians(Start.latitude)) * sin(radians(End.latitude))
+        + cos(radians(Start.latitude)) * cos(radians(End.latitude))
+        * cos(abs(radians(End.longitude) - radians(Start.longitude)))
+    )
+    distance = distance * RT
+
+    return distance
