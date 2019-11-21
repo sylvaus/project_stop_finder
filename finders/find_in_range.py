5cefc5978e3915016aee539a006efa82a9abac70
jaassb <jamal.assi89@hotmail.com>
2019-11-21 12:32:00 -0500

    Implemented find in range method

--- finders/find_in_range.py
+++ finders/find_in_range.py
@@ -1,10 +1,9 @@
 from typing import List
 
-from finders.gps import GPSPosition
+from finders.gps import GPSPosition, gps_distance
 from finders.stops import Stop
 
 
 def find_stops_in_range(
-        stops: List[Stop], gps: GPSPosition, range_: float, regex: bool = False
-) -> List[Stop]:
-    pass
+        stops: List[Stop], gps: GPSPosition, range_: float) -> List[Stop]:
+    return filter(lambda stop_in_range: gps_distance(gps, stop.gps) <= range_, stops)
