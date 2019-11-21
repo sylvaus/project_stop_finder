d8cf91a7c93df7b9059ca8a0a3933470b0f73d36
jaassb <jamal.assi89@hotmail.com>
2019-11-21 13:07:58 -0500

    Typo fixed

--- finders/find_in_range.py
+++ finders/find_in_range.py
@@ -6,4 +6,4 @@ from finders.stops import Stop
 
 def find_stops_in_range(
         stops: List[Stop], gps: GPSPosition, range_: float) -> List[Stop]:
-    return [stop for stop in stop if gps_distance(gps, stop.gps) <= range_]
+    return [stop for stop in stops if gps_distance(gps, stop.gps) <= range_]
