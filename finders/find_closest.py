0942edb97f769024783e0f12db0962887399587e
freddyfok <ffok@intranet.cs>
2019-11-21 19:10:01 -0500

    Implemented find_closest_stop function and unit test

--- finders/find_closest.py
+++ finders/find_closest.py
@@ -1,8 +1,14 @@
 from typing import List
 
-from finders.gps import GPSPosition
+from finders.gps import GPSPosition, gps_distance
 from finders.stops import Stop
 
 
 def find_closest_stop(stops: List[Stop], gps: GPSPosition) -> Stop:
-    pass
+    closest_stop = stops[0]
+    closest_distance = gps_distance(gps, closest_stop.gps)
+    for stop in stops[1:]:
+        if gps_distance(gps, stop.gps) < closest_distance:
+            closest_distance = gps_distance(gps, stop.gps)
+            closest_stop = stop
+    return closest_stop
