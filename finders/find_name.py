d0bc3aacfce435be3f967727e963a989f795e67a
Nadia <nadezda.b2007@gmail.com>
2019-11-26 20:22:18 -0500

    deleted the old branch, trying to re-commit

--- finders/find_name.py
+++ finders/find_name.py
@@ -1,7 +1,30 @@
 from typing import List
-
 from finders.stops import Stop
+import re
 
 
 def find_stops_matching_name(stops: List[Stop], name: str, regex: bool = False) -> List[Stop]:
-    pass
+    found = False
+    results = list()
+    if not regex:
+        for element in stops:
+            if element.name == name:
+                print("Your stop is: ", element)
+                found = True
+                return element
+        if not found:
+            print("Sorry, this stop doesn't exist. Try regex=True")
+    else:
+        for element in stops:
+            match = re.search(name, element.name, re.IGNORECASE)
+            if match:
+                results.append(element)
+                print("Your stop is: ", element)
+        if not results:
+            print("Sorry, can't find anything")
+        else:
+            return results
+
+
+
+
