627a8558f408027aed519c0a1e5d1f319db33ebf
jaassb <jamal.assi89@hotmail.com>
2019-11-27 22:52:10 -0500

    test - final draft

--- tests/test_find_in_range.py
+++ tests/test_find_in_range.py
@@ -1,11 +1,26 @@
-from pytest import mark
+import pytest
 
-TEST_VALUES = []
+from finders.find_in_range import find_stops_in_range
+from finders.gps import GPSPosition
+from finders.stops import Stop
 
-TEST_NAMES = []
+SAMPLE_STOPS = [
+    Stop("location_sw", GPSPosition(-10, -10))
+    , Stop("location_se", GPSPosition(10, -10))
+    , Stop("location_ne", GPSPosition(10, 10))
+    , Stop("location_nw", GPSPosition(-10, 10))
+]
 
 
-@mark.parametrize("", TEST_VALUES, ids=TEST_NAMES)
-def test_find_in_range():
+TEST_DATA = [
+    (GPSPosition(1, 1), Stop("location_ne", GPSPosition(10, 10)))
+    , (GPSPosition(1, -1), Stop("location_se", GPSPosition(10, -10)))
+    , (GPSPosition(-1, -1), Stop("location_sw", GPSPosition(-10, -10)))
+    , (GPSPosition(-1, 1), Stop("location_nw", GPSPosition(-10, 10)))
+]
 
-    assert
+
+@pytest.mark.parametrize("current_gps_location, range_value,expected_stops", TEST_DATA)
+def test_find_in_range(current_gps_location, range_value, expected_stops):
+    stop_in_range = find_stops_in_range(SAMPLE_STOPS, current_gps_location, range_value)
+    assert stop_in_range == expected_stops
