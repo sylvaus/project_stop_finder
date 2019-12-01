080a88c457b4d406ac200825b646b4cc3a4bf34e
Nadia <nadezda.b2007@gmail.com>
2019-12-01 18:38:37 -0500

    rework

--- finders/find_name.py
+++ finders/find_name.py
@@ -4,27 +4,15 @@ import re
 
 
 def find_stops_matching_name(stops: List[Stop], name: str, regex: bool = False) -> List[Stop]:
-    found = False
     results = list()
     if not regex:
         for element in stops:
             if element.name == name:
-                print("Your stop is: ", element)
-                found = True
-                return element
-        if not found:
-            print("Sorry, this stop doesn't exist. Try regex=True")
+                results.append(element)
     else:
         for element in stops:
             match = re.search(name, element.name, re.IGNORECASE)
             if match:
                 results.append(element)
-                print("Your stop is: ", element)
-        if not results:
-            print("Sorry, can't find anything")
-        else:
-            return results
-
-
-
+    return results
 
