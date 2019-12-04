8476b782357d999c59d39772e91e8deb7c9759a1
jaassb <jamal.assi89@hotmail.com>
2019-12-03 22:31:30 -0500

    Typo fixed

--- tests/test_find_in_range.py
+++ tests/test_find_in_range.py
@@ -5,19 +5,19 @@ from finders.gps import GPSPosition
 from finders.stops import Stop
 
 SAMPLE_STOPS = [
-    Stop("Bibliothèque du Boisé", GPSPosition(45.506314, -73.711494))
-    , Stop("Cavendish / Poirier", GPSPosition(45.504783, -73.704418))
+    Stop("Cavendish / Poirier", GPSPosition(45.506314, -73.711494))
+    , Stop("Bibliothèque du Boisé", GPSPosition(45.504783, -73.704418))
     , Stop("Thimens / Alexis-Nihon", GPSPosition(45.508977, -73.698584))
     , Stop("Marcel-Laurin / Beaudet", GPSPosition(45.506968, -73.681766))
 ]
 
 
 TEST_DATA = [
-    (GPSPosition(45.498265, -73.707559), 950, [Stop("Bibliothèque du Boisé", GPSPosition(45.506314, -73.711494))
-                                                , Stop("Cavendish / Poirier", GPSPosition(45.504783, -73.704418))])
-    , (GPSPosition(45.498265, -73.707559), 770, [Stop("Cavendish / Poirier", GPSPosition(45.504783, -73.704418))])
-    , (GPSPosition(45.498265, -73.707559), 1400, [Stop("Bibliothèque du Boisé", GPSPosition(45.506314, -73.711494))
-                                                    , Stop("Cavendish / Poirier", GPSPosition(45.504783, -73.704418))
+    (GPSPosition(45.498265, -73.707559), 950, [Stop("Cavendish / Poirier", GPSPosition(45.506314, -73.711494))
+                                                , Stop("Bibliothèque du Boisé", GPSPosition(45.504783, -73.704418))])
+    , (GPSPosition(45.498265, -73.707559), 770, [Stop("Bibliothèque du Boisé", GPSPosition(45.504783, -73.704418))])
+    , (GPSPosition(45.498265, -73.707559), 1400, [Stop("Cavendish / Poirier", GPSPosition(45.506314, -73.711494))
+                                                , Stop("Bibliothèque du Boisé", GPSPosition(45.504783, -73.704418))
                                                     , Stop("Thimens / Alexis-Nihon", GPSPosition(45.508977, -73.698584))])
     , (GPSPosition(45.498265, -73.707559), 2250, SAMPLE_STOPS)
 ]
